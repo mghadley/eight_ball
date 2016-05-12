b11d6950ef9a4c5302e96d1b14e69383dc4b5dab
mghadley <matthew.g.hadley@gmail.com>
2016-05-12 13:47:02 -0600

    first and possibly last commit

---
+++ magic_8_ball.rb
@@ -0,0 +1,58 @@
+ANSWER_BANK = ["Maybe", "Always wear sunscreen", "Your mom", "Ha! You wish",
+					 "That's a stupid question", "42"]
+
+#define_answers = -> {@answers = ANSWER_BANK}
+
+def define_answers
+	@answers = ANSWER_BANK.clone
+end
+
+def add_answers
+	user_answer = gets.strip
+	if @answers.include? user_answer
+		puts "This answer already exists"
+	else
+		@answers << user_answer
+		puts "\nAnswer added successfully", "\n"
+	end
+end
+
+def get_input
+	input = gets.strip.downcase
+	case input
+		when 'quit'
+			puts "Goodbye"
+			exit 0
+		when 'print_answers', 'print answers'
+			puts "\n", @answers, "\n"
+			main
+		when 'add_answers', 'add answers'
+			puts "\nWhat answer would you like to add?"
+			print ">"
+			add_answers
+			main
+		when 'reset_answers', 'reset answers'
+			define_answers
+			puts "\nAnswers reset successfully", "\n"
+			main
+		else
+			input
+	end
+end
+			
+
+def main
+	while true
+		puts "\nWhat are you wondering?"
+		print ">"
+		user_question = get_input
+		puts "\n", @answers.sample, "\n"
+	end
+end
+
+define_answers
+puts "\nWeclome to the Magic Eight Ball!"
+puts "Can you find the easter egg commands?"
+main
+
+
